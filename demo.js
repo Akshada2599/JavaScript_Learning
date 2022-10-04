8d9f393d7afb49d5970e8e85f7c27ea10de9e8e0
Akshada2599 <akshadapangarkar111@gmail.com>
2022-10-04 14:17:45 +0530

    First Program execution in JS
    First Program execution in JS

---
+++ demo.js
@@ -0,0 +1,11 @@
+//this is my first program
+console.log("Hello");
+console.log("My name is AKshada");
+console.info("this is info ");  //info means information
+
+console.warn("i am not that bad guy ");  //warn means warning
+console.error(" Opps failed to excuted");
+
+
+
+console.log(" 99999 ");
