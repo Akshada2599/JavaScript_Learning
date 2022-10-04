0930d66285e01ca941ea4520732b8be746f59eeb
Akshada2599 <akshadapangarkar111@gmail.com>
2022-10-04 20:29:09 +0530

    added console methods
    added console method

---
+++ Assignment1.js
@@ -0,0 +1,6 @@
+console.log("My name is Akshada Pangarkar");
+console.log("My Hobbies : 1.Dancing 2.Cooking 3Drawing");
+console.info("My strength : positive attitude,honestly,disciplined ");
+console.warn("My weakness : Public Speaking,emotional,insecure");
+console.log(30+50);
+console.log(49*479);
