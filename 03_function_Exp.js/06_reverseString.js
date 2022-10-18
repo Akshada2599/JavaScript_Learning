2fd87d89911066c2d1b1e4076ff85c0c5ce10e73
Akshada2599 <akshadapangarkar111@gmail.com>
2022-10-18 20:05:49 +0530

    reverse string
    reverse string

---
+++ 03_function_Exp.js/06_reverseString.js
@@ -0,0 +1,13 @@
+var myName = "AKshada";
+//i just want to print 10 to 1
+var lenthStr = myName.length-1;
+var reverseStr ="";
+
+for(let index =lenthStr; index>=0; index--){
+    console.log(myName.charAt(index));
+    reverseStr = reverseStr + myName.charAt(index);//""+"t"
+    
+}
+console.log(reverseStr);
+
+//0+!+2+3+4
